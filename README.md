0b91c840032bd3fdad2bc617266fe9406a1619c3
Mighty <mightykiller2b@gmail.com>
2015-08-01 20:12:48 +0200

    Ootfy is free strong authentication software.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Ootfy
+# OOTFY
 
-Ootfy is free strong authentication software. With Ootfy you can authenticate your client safety even if they use short passwords.
+OOTFY is free strong authentication software. With OOTFY you can authenticate 
+your client safety even if they use short passwords.
 
 ## License
 
@@ -8,20 +9,30 @@ This software is licensed under the terms of the MIT license.
 
 ## Installation
 
-This is the client part, server part is in an other repository.
-For the moment you have to install Ootfy by your self.
+This is the client part, server part is in an other repository :
+https://github.com/V-Paranoiaque/OOTFY-server
+For the moment you have to install OOTFY by your self.
 
-Just install Cordova and copy all files. After you will be able to compile mobile applications.
+For the moment I don't provide installation by stores.
+Just install Cordova and copy all files. 
+After you will be able to compile mobile applications.
 
 ## Platform support
 
 * Android
+* Blackberry 10
 * Firefox OS
-* Ios
+* iOS
 
 ## Usage
 
-When a user connects to your service, you ask him an additional code. This code will be generated simultaneously on your server and on the user's mobile. Both codes will be compared to guaranteed a secured access. You can add a mechanism against the brute force to increase security.
+Don't forget to configure OOTFY in the configuration manu.
 
-With Ootfy, even if a password stolen your users are safe. An analysis of server's logs may detect intrusion attempt.
+When a user connects to your service, you ask him an additional code. This code 
+will be generated simultaneously on your server and on the user's mobile. Both 
+codes will be compared to guaranteed a secured access. You can add a mechanism 
+against the brute force to increase security.
+
+With OOTFY, even if a password stolen your users are safe. An analysis of 
+server's logs may detect intrusion attempt.
 
---
+++ www/js/index.js
@@ -0,0 +1,260 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var url = document.URL.split('/');
+        var current = url[url.length-1];
+        if(current == 'index.html') {
+            setTimeout(function(){
+              document.location.href="home.html";
+            }, 500);
+        }
+        else if(current == 'conf.html') {
+            load_config();
+        }
+        else if(current == 'token.html') {
+            display_token();
+        }
+    }
+};
+
+app.initialize();
+
+/*** Menu ***/
+$("#menu-home").touchstart(function() {
+	   document.location.href="home.html"
+});
+$("#menu-conf").touchstart(function() {
+	   document.location.href="conf.html"
+});
+
+/*** Home ***/
+$("#pin_0").touchstart(function() {
+	   add_pin("0");
+});
+
+$("#pin_1").touchstart(function() {
+     add_pin("1");
+});
+
+$("#pin_2").touchstart(function() {
+     add_pin("2");
+});
+
+$("#pin_3").touchstart(function() {
+     add_pin("3");
+});
+
+$("#pin_4").touchstart(function() {
+     add_pin("4");
+});
+
+$("#pin_5").touchstart(function() {
+     add_pin("5");
+});
+
+$("#pin_6").touchstart(function() {
+     add_pin("6");
+});
+
+$("#pin_7").touchstart(function() {
+     add_pin("7");
+});
+
+$("#pin_8").touchstart(function() {
+     add_pin("8");
+});
+
+$("#pin_9").touchstart(function() {
+     add_pin("9");
+});
+
+function add_pin(num) {
+  if($("#pin-area").val().length < 4) {
+    $("#pin-area").val($("#pin-area").val()+""+num);
+  }
+}
+
+$("#pin_ok").touchstart(function() {
+	var pin = $("#pin-area").val();
+	
+	if(pin != '') {
+		localStorage.pin = pin;
+		document.location.href="token.html"
+	}
+});
+
+$("#pin_delete").touchstart(function() {
+	   $("#pin-area").val('');
+});
+
+/*** Conf ***/
+$("#conf-save").touchstart(function() {
+  var userid = $("#conf-userid").val();
+  if(userid != "" && userid != null) {
+    localStorage.userid = userid;
+  }
+  
+  var domain = $("#conf-domain").val();
+  if(domain != "" && domain != null) {
+    localStorage.domain = domain;
+  }
+  
+  var key = $("#conf-key").val();
+  if(key == 4 || key == 8 || 
+     key == 16 || key == 32) {
+    localStorage.key_size = key;
+  }
+  
+  var validity = $("#conf-validity").val();
+  if(validity == 15 || validity == 30 || validity == 60 || 
+     validity == 90 || validity == 120) {
+    localStorage.validity = validity;
+  }
+  
+  $("#conf-error").show();
+  
+  load_config();
+});
+
+function load_config() {
+  
+  var userid = localStorage.userid;
+  if(userid != "" && userid != null) {
+    $("#conf-userid").val(userid);
+  }
+  
+  var domain = localStorage.domain;
+  if(domain != "" && domain != null) {
+    $("#conf-domain").val(domain);
+  }
+  
+  var key = localStorage.key_size;
+  if(key == 4  || key == 8 || 
+     key == 16 || key == 32) {
+     $("#conf-key").val(key);
+     $("#conf-key").selectmenu('refresh', true);
+  }
+  
+  var validity = localStorage.validity;
+  if(validity == 15 || validity == 30 || validity == 60 || 
+     validity == 90 || validity == 120) {
+    $("#conf-validity").val(validity);
+    $("#conf-validity").selectmenu('refresh', true);
+  }
+}
+
+/*** Token ***/
+
+$("#token-back").touchstart(function() {
+	   document.location.href="home.html"
+});
+
+function generate_token(base_time) {
+
+  var user_id = localStorage.user_id;
+  
+  if(user_id == null) {
+    user_id = '';
+  }
+  
+  var domain_key = localStorage.domain_key;
+  if(domain_key == null) {
+    domain_key = '';
+  }
+  
+  var key_size = localStorage.key_size;
+  
+  if(key_size != 4 && key_size != 8 && 
+   key_size != 16 && key_size != 32) {
+    key_size = 4;
+  }
+  
+  var pin = $("#pin").val();
+  
+  if(pin == null) {
+    pin = '';
+  }
+  
+  var shaPin = new jsSHA(pin+base_time, "TEXT");
+  var shaObj = new jsSHA(user_id+shaPin.getHash("SHA-512", "HEX")+domain_key, "TEXT");
+  
+  return shaObj.getHash("SHA-512", "HEX").substring(0, key_size);
+}
+
+function display_token() {
+
+  var timestamp = Math.round(+new Date() / 1000);
+  var period    = localStorage.period;
+  var token     = generate_token(timestamp);
+  var val       = 0;
+  
+  if(period != 15 && period != 30 && period != 60 && 
+     period != 90 && period != 120) {
+    period = 60;
+  }
+  
+  var increment = 360/period;
+  $("#token").val(format_token(token));
+  
+  window.setInterval(function(){
+    timestamp = Math.round(+new Date() / 1000);
+    val = (period-timestamp%period);
+    
+    if(val == period) {
+      token = generate_token(timestamp);
+      $("#token").val(format_token(token));
+    }
+    
+    $('#progress-bar').val(val * increment);
+    $('#progress-bar').slider('refresh');
+  }, 1000);
+}
+
+function format_token(token) {
+  var back = '';
+  var i;
+  for(i=0;i<token.length;i++) {
+    if(i > 0 && i%4 == 0) {
+      back = back + ' ';
+    }
+    back = back + token[i];
+  }
+  return back;
+}
+
