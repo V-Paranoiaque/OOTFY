7858309647ed0a98038b533289a26698dffe547d
Mighty <mightykiller2b@gmail.com>
2015-08-06 20:32:45 +0200

    0.0.3 : Update design

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -60,6 +42,7 @@ app.initialize();
 $("#menu-home").touchstart(function() {
 	   document.location.href="home.html"
 });
+
 $("#menu-conf").touchstart(function() {
 	   document.location.href="conf.html"
 });
@@ -111,9 +94,21 @@ function add_pin(num) {
   }
 }
 
+$("#pin_correct").touchstart(function() {
+  var i, length;
+  var pin = $("#pin-area").val();
+  var newpin = '';
+  if(pin.length > 0) {
+    length = pin.length - 1;
+    for(i=0;i<length;i++) {
+      newpin = newpin + ''+ pin[i];
+    }
+    $("#pin-area").val(newpin);
+  }
+});
+
 $("#pin_ok").touchstart(function() {
 	var pin = $("#pin-area").val();
-	alert(pin);
 	if(pin != '') {
 		localStorage.pin = pin;
 		document.location.href="token.html"
